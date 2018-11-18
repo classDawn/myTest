efeda45ff1acec6aa3c607b3b18455d4b29bb819
chencong <dawncong@163.com>
2018-11-18 17:48:36 +0800

    ---添加注释

--- dawncloud-consumer-feign/src/main/java/dawn/cloud/consumer/feign/controller/ConsumerController_Feign.java
+++ dawncloud-consumer-feign/src/main/java/dawn/cloud/consumer/feign/controller/ConsumerController_Feign.java
@@ -20,11 +20,21 @@ public class ConsumerController_Feign {
 	public Dept get(@PathVariable("id") Long id) {
 		return this.dept_feignService.get(id);
 	}
+
+	/**
+	 *
+	 * @return
+	 */
 	@RequestMapping(value = "/consumer/dept/list")
 	public List<Dept> list() {
 		return this.dept_feignService.list();
 	}
 
+	/**
+	 * 注意这个操作
+	 * @param dept
+	 * @return
+	 */
 	@RequestMapping(value = "/consumer/dept/add")
 	public Object add(Dept dept) {
 		return this.dept_feignService.add(dept);
